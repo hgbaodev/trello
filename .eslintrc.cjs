7d806792d9f6700dcbce13dd87b8eea7a2deca1e
hgbaodev <120194990+hgbaodev@users.noreply.github.com>
2023-07-14 01:39:40 +0700

    Setup isntall project mui and fix eslint index.html

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,6 +6,7 @@ module.exports = {
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended'
   ],
+  ignorePatterns: ['**/*.html'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: [
