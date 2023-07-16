c76e2753bffc04535a758e58d6eff19161b7040f
hgbaodev <120194990+hgbaodev@users.noreply.github.com>
2023-07-16 13:45:44 +0700

    setup layout trello

--- src/theme.js
+++ src/theme.js
@@ -3,6 +3,10 @@ import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
 
 // Create a theme instance.
 const theme = extendTheme({
+  trello: {
+    appBarHeight: '48px',
+    boardBardHeight: '58px'
+  },
   colorSchemes: {
     light: {
       palette: {
@@ -17,7 +21,6 @@ const theme = extendTheme({
       }
     }
   }
-  // ...other properties
 })
 
 export default theme
