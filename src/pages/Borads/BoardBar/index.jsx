deaa7a7b266742580d6d66f478f9bc5c109c9f43
hgbaodev <120194990+hgbaodev@users.noreply.github.com>
2023-07-18 00:59:23 +0700

    add boardBar

--- src/pages/Borads/BoardBar/index.jsx
+++ src/pages/Borads/BoardBar/index.jsx
@@ -1,15 +1,106 @@
 import Box from '@mui/material/Box'
+import Chip from '@mui/material/Chip'
+import DashboardIcon from '@mui/icons-material/Dashboard'
+import VpnLockIcon from '@mui/icons-material/VpnLock'
+import AddToDriveIcon from '@mui/icons-material/AddToDrive'
+import BoltIcon from '@mui/icons-material/Bolt'
+import FilterListIcon from '@mui/icons-material/FilterList'
+import Avatar from '@mui/material/Avatar'
+import AvatarGroup from '@mui/material/AvatarGroup'
+import { Button, Tooltip } from '@mui/material'
+import PersonAddIcon from '@mui/icons-material/PersonAdd'
+
+const MENU_STYLES = {
+  color: 'primary.main',
+  bgcolor: 'white',
+  border: 'none',
+  paddingX: '5px',
+  borderRadius: '5px',
+  '& .MuiSvgIcon-root': {
+    color: 'primary.main'
+  },
+  '&:hover': {
+    bgcolor: 'primary.50'
+  }
+}
 
 const BoardBar = () => {
   return (
     <Box sx={{
-      backgroundColor: 'primary.dark',
       width: '100%',
       height: (theme) => theme.trello.boardBardHeight,
       display: 'flex',
-      alignItems: 'center'
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      gap: 2,
+      paddingX: 2,
+      overflowX: 'auto',
+      borderTop: '1px solid #00bfa5'
     }}>
-      Board Bar
+      <Box sx={{
+        display: 'flex',
+        alignItems: 'center',
+        gap: 2
+      }}>
+        <Chip
+          sx={MENU_STYLES}
+          icon={<DashboardIcon />} label="hgbaodev FULL Stack Board"
+          clickable
+          onClick={() => {}}
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<VpnLockIcon />} label="Pulbic/Private Workspaces"
+          clickable
+          onClick={() => {}}
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<AddToDriveIcon />} label="Add To Google Drive"
+          clickable
+          onClick={() => {}}
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<BoltIcon />} label="Automation "
+          clickable
+          onClick={() => {}}
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<FilterListIcon />} label="Filters"
+          clickable
+          onClick={() => {}}
+        />
+      </Box>
+      <Box sx={{
+        display: 'flex',
+        alignItems: 'center',
+        gap: 2
+      }}>
+        <Button variant="outlined" startIcon={<PersonAddIcon/>}>Invite</Button>
+        <AvatarGroup
+          max={7}
+          sx={{
+            '& .MuiAvatar-root': {
+              width: 34,
+              height: 34,
+              fontSize: 16
+            }
+          }}
+        >
+          <Tooltip title="hgbaodev">
+            <Avatar alt="hgbaodev" src="https://avatars.githubusercontent.com/u/120194990?v=4" />
+          </Tooltip>
+          <Tooltip title="hgbaodev">
+            <Avatar alt="hgbaodev" src="https://avatars.githubusercontent.com/u/120194990?v=4" />
+          </Tooltip><Tooltip title="hgbaodev">
+            <Avatar alt="hgbaodev" src="https://avatars.githubusercontent.com/u/120194990?v=4" />
+          </Tooltip><Tooltip title="hgbaodev">
+            <Avatar alt="hgbaodev" src="https://avatars.githubusercontent.com/u/120194990?v=4" />
+          </Tooltip>
+        </AvatarGroup>
+      </Box>
     </Box>
   )
 }
