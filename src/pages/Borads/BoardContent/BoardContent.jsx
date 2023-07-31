62b009f656a54c5b4405628437963844f06e7ffc
hgbaodev <120194990+hgbaodev@users.noreply.github.com>
2023-08-01 00:57:13 +0700

    hanleDragOver

--- src/pages/Borads/BoardContent/BoardContent.jsx
+++ src/pages/Borads/BoardContent/BoardContent.jsx
@@ -12,6 +12,7 @@ import {
 } from '@dnd-kit/core'
 import { arrayMove } from '@dnd-kit/sortable'
 import { useEffect, useState } from 'react'
+import { cloneDeep } from 'lodash'
 
 import Column from './ListColumns/Column/Column'
 import Card from './ListColumns/Column/ListCards/Card.jsx/Card'
@@ -46,17 +47,81 @@ const BoardContent = ({ board }) => {
     setOrderedColumns(mapOrder(board?.columns, board?.columnOrderIds, '_id'))
   }, [board])
 
+  // Tìm cái column theo cardId
+  const findColumnByCardId = (cardId) => {
+    return orderedColumns.find(column => column?.cards?.map(card => card._id)?.includes(cardId))
+  }
+
   const handleDragStart = (event) => {
     setActiveDragItemmId(event?.active?.id)
     setActiveDragItemmType(event?.active?.data?.current?.columnId ? ACTIVE_DRAG_ITEM_TYPE.CARD : ACTIVE_DRAG_ITEM_TYPE.COLUMN)
     setActiveDragItemmData(event?.active?.data?.current)
   }
 
+  // Trigger trong quá trình kéo 1 phần từ
+  const handleDrapOver = (event) => {
+    if (activeDragItemType === ACTIVE_DRAG_ITEM_TYPE.COLUMN) return
+    // console.log('handleDropOver', event)
+    const { active, over } = event
+    if (!active || !over) return
+
+    // activeDraggingCard: là cái card đang được kéo
+    const { id: activeDraggingCardId, data: { current: activeDraggingCardData } } = active
+    // overcard: là cái card đang tương tác trên hoặc dưới so với cái card được kéo ở trên
+    const { id: overCardId } = over
+
+    // Tìm 2 cái column thuộc cái card id
+    const activeColumn = findColumnByCardId(activeDraggingCardId)
+    const overColumn = findColumnByCardId(overCardId)
+
+    if (!activeColumn || !overColumn ) return
+
+    if (activeColumn._id !== overColumn._id) {
+      setOrderedColumns(prevColumns => {
+        // Tìm vị trí (index) của cái overCard trong column đích
+        const overCardIndex = overColumn?.cards?.findIndex(card => card._id === overCardId)
+
+        let newCardIndex
+
+        const isBelowOverItem = active.rect.current.translated && active.rect.current.translated.top > over.rect.top + over.rect.height
+        const modifier = isBelowOverItem ? 1 : 0
+
+        newCardIndex = overCardIndex >= 0 ? overCardIndex + modifier : overColumn?.cards.length + 1
+
+        const nextColumns = cloneDeep([...prevColumns])
+        const nextActiveColumns = nextColumns.find(column => column._id === activeColumn._id )
+        const nextOverColumns = nextColumns.find(column => column._id === overColumn._id )
+
+        // Cloumn cũ
+        if (nextActiveColumns) {
+          //Xóa card ở cải column active
+          nextActiveColumns.cards = nextActiveColumns.cards.filter(card => card._id !== activeDraggingCardId)
+          // Cập nhật lại mảng card orderIds
+          nextActiveColumns.cardOrderIds = nextActiveColumns.cards.map(card => card._id)
+        }
+        // Column mới
+        if (overColumn) {
+          // Kiểm tra xem cái card đang kéo có tồn tại ở  overColumn hay chưa nếu có thì xóa nó trước đi
+          nextOverColumns.cards = nextOverColumns.cards.filter(card => card._id !== activeDraggingCardId)
+          //Thêm cái card mới vào over column
+          nextOverColumns.cards = nextOverColumns.cards.toSpliced(newCardIndex, 0, activeDraggingCardData)
+          // Cập nhật lại mảng card orderIds
+          nextOverColumns.cardOrderIds = nextOverColumns.cards.map(card => card._id)
+        }
+
+        return nextColumns
+      })
+    }
+  }
 
   const handleDrapEnd = (event) => {
+    // console.log(event)
+    if (activeDragItemType == ACTIVE_DRAG_ITEM_TYPE.CARD) {
+      console.log('Hành động kéo thả Card - Tạm thời khoogn làm gì cả')
+      return
+    }
     const { active, over } = event
-
-    if (!over) return
+    if (!active || !over) return
 
     if (active.id !== over.id) {
       const oldIndex = orderedColumns.findIndex(c => c._id == active.id)
@@ -76,6 +141,7 @@ const BoardContent = ({ board }) => {
     <DndContext
       sensors={sensors}
       onDragStart={handleDragStart}
+      onDragOver={handleDrapOver}
       onDragEnd={handleDrapEnd}>
       <Box sx={{
         bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'),
