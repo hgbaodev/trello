98311302cd5c9c7730d605a981936bc2b37ab17a
Hoàng Gia Bảo <120194990+hgbaodev@users.noreply.github.com>
2023-08-21 07:23:00 +0700

    add boilerplate server

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,15 +1,32 @@
-// const express = require("express");
-import express from "express";
-const app = express();
-const hostname = "localhost";
-const port = 8017;
+/**
+ * Updated by hgbaodev.com's author on August 17 2023
+ * YouTube: https://youtube.com/@hgbaodev
+ * "A bit of fragrance clings to the hand that gives flowers!"
+ */
 
-app.get("/", function (req, res) {
-  res.send("<h1>Hello World Nodejs hgbaodev</h1>");
-});
+import express from 'express'
+import { mapOrder } from '~/utils/sorts.js'
+
+const app = express()
+
+const hostname = 'localhost'
+const port = 8017
+
+app.get('/', (req, res) => {
+  // Test Absolute import mapOrder
+  console.log(mapOrder(
+    [{ id: 'id-1', name: 'One' },
+      { id: 'id-2', name: 'Two' },
+      { id: 'id-3', name: 'Three' },
+      { id: 'id-4', name: 'Four' },
+      { id: 'id-5', name: 'Five' }],
+    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
+    'id'
+  ))
+  res.end('<h1>Hello World hgbaodev!</h1><hr>')
+})
 
 app.listen(port, hostname, () => {
-  console.log(
-    `Hello hgbaodev, I'am running server at http://${hostname}:${port}`
-  );
-});
+  // eslint-disable-next-line no-console
+  console.log(`Hello hgbaodev, I am running at http://${ hostname }:${ port }/`)
+})
