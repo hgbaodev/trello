b4eb06bd3601176d875d52b189fdbf1f3aa4617e
Hoàng Gia Bảo <120194990+hgbaodev@users.noreply.github.com>
2023-09-15 23:27:30 +0700

    install_md

--- Server/src/config/mongodb.js
+++ Server/src/config/mongodb.js
@@ -1,4 +1,5 @@
 //username: hgbaodev
 //password: aomdc0LN2VlGz0hM
+//DatabaseName: trello-hgbaodev
 
 const MONGODB_URL = "mongodb+srv://hgbaodev:aomdc0LN2VlGz0hM@cluster0-hgbaodev.imugkwu.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp"
