ba32bac2053b5fe376d2f76d44b09559e57173da
Hoàng Gia Bảo <120194990+hgbaodev@users.noreply.github.com>
2023-09-15 22:52:07 +0700

    create mogodb

---
+++ Server/src/config/mongodb.js
@@ -0,0 +1,4 @@
+//username: hgbaodev
+//password: aomdc0LN2VlGz0hM
+
+const MONGODB_URL = "mongodb+srv://hgbaodev:aomdc0LN2VlGz0hM@cluster0-hgbaodev.imugkwu.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp"
