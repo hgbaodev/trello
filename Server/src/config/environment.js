1b48ed74274d4e029a27f4d252724ba034fb2715
Hoàng Gia Bảo <120194990+hgbaodev@users.noreply.github.com>
2023-09-16 13:47:43 +0700

    env

---
+++ Server/src/config/environment.js
@@ -0,0 +1,15 @@
+/**
+ * Updated by hgbaodev.com's author on August 17 2023
+ * YouTube: https://youtube.com/@hgbaodev
+ * "A bit of fragrance clings to the hand that gives flowers!"
+ */
+import 'dotenv/config'
+
+export const env = {
+  MONGODB_URI: process.env.MONGODB_URI,
+  DATABASE_NAME: process.env.DATABASE_NAME,
+  APP_HOST: process.env.APP_HOST,
+  APP_PORT: process.env.APP_PORT,
+  AUTHOR: process.env.AUTHOR
+
+}
