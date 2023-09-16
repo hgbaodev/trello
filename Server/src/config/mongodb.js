b0f67828df9ed529cdaa308463f90fb1fc0110f5
Hoàng Gia Bảo <120194990+hgbaodev@users.noreply.github.com>
2023-09-16 12:10:50 +0700

    add connect-db

--- Server/src/config/mongodb.js
+++ Server/src/config/mongodb.js
@@ -2,4 +2,29 @@
 //password: aomdc0LN2VlGz0hM
 //DatabaseName: trello-hgbaodev
 
-const MONGODB_URL = "mongodb+srv://hgbaodev:aomdc0LN2VlGz0hM@cluster0-hgbaodev.imugkwu.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp"
+const MONGODB_URL = 'mongodb+srv://hgbaodev:aomdc0LN2VlGz0hM@cluster0-hgbaodev.imugkwu.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp'
+const DATABASE_NAME = 'trello-hgbaodev'
+
+import { MongoClient, ServerApiVersion } from 'mongodb'
+
+let trelloDatabaseInstance = null
+
+//Khởi tạo 1 đối tượng instance để connect đến MogoDb
+const mongoClientInstance = new MongoClient(MONGODB_URL, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
+// Kết nối tới Database
+export const CONNECT_DB = async () => {
+  await mongoClientInstance.connect()
+
+  trelloDatabaseInstance = mongoClientInstance.db(DATABASE_NAME)
+}
+
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance) throw new Error('Must connect to Database first')
+  return trelloDatabaseInstance
+}
--- Server/src/server.js
+++ Server/src/server.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /**
  * Updated by hgbaodev.com's author on August 17 2023
  * YouTube: https://youtube.com/@hgbaodev
@@ -5,29 +6,46 @@
  */
 
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import { CONNECT_DB, GET_DB } from '~/config/mongodb'
 
-const app = express()
 
-const hostname = 'localhost'
-const port = 8017
+const START_SERVER = () => {
+  const app = express()
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  // eslint-disable-next-line no-console
-  console.log(mapOrder(
-    [{ id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' }],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World hgbaodev!</h1><hr>')
-})
+  const hostname = 'localhost'
+  const port = 8017
+  app.get('/', async (req, res) => {
+    console.log(await GET_DB().listCollections().toArray())
+    res.end('<h1>Hello World hgbaodev!</h1><hr>')
+  })
+  app.listen(port, hostname, () => {
+    // eslint-disable-next-line no-console
+    console.log(`3. Hello hgbaodev, Backend server is running successfully at host:  http://${ hostname }:${ port }/`)
+  })
+}
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello hgbaodev, I am running at http://${ hostname }:${ port }/`)
-})
+// Khi connnect database thành công thì mới START_SERVER
+// Immediately-invoked / Anonymous Async Function (IIFE)
+(async () => {
+  try {
+    console.log('1. Connecting to MogoDB Cloud Atlas')
+    await CONNECT_DB()
+    console.log('2. Connected to MogoDB Cloud Atlas')
+
+    // Khởi động server sau khi connect databse thành công
+    START_SERVER()
+  } catch (error) {
+    console.error(error)
+    process.exit(0)
+  }
+})()
+
+
+// CONNECT_DB()
+//   .then(() => console.log('2. Connected to MogoDB Cloud Atlas'))
+//   .then(() => START_SERVER())
+//   .catch(error => {
+//     console.error(error)
+//     process.exit(0)
+//   })
+
