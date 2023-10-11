6e8ff3a3948bbc2ddea081bf7f2fed30f2262193
Hoàng Gia Bảo <120194990+hgbaodev@users.noreply.github.com>
2023-10-11 21:32:15 +0700

    clear format utils

--- Client/src/utils/getColors.js
+++ Client/src/utils/getColors.js
@@ -2,19 +2,19 @@
 
 const getColors = (theme, color) => {
   switch (color) {
-    case 'secondary':
-      return theme.palette.secondary;
-    case 'error':
-      return theme.palette.error;
-    case 'warning':
-      return theme.palette.warning;
-    case 'info':
-      return theme.palette.info;
-    case 'success':
-      return theme.palette.success;
-    default:
-      return theme.palette.primary;
+  case 'secondary':
+    return theme.palette.secondary
+  case 'error':
+    return theme.palette.error
+  case 'warning':
+    return theme.palette.warning
+  case 'info':
+    return theme.palette.info
+  case 'success':
+    return theme.palette.success
+  default:
+    return theme.palette.primary
   }
-};
+}
 
-export default getColors;
+export default getColors
