705d863f7c69762411f40ca4d2bfff0d2b18d9e1
Hoàng Gia Bảo <120194990+hgbaodev@users.noreply.github.com>
2023-12-16 14:10:14 +0700

    add cors

--- Server/src/server.js
+++ Server/src/server.js
@@ -6,6 +6,7 @@
  */
 
 import express from 'express'
+import cors from 'cors'
 import exitHook from 'async-exit-hook'
 import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
 import { env } from '~/config/environment'
@@ -15,6 +16,8 @@ import { errorHandlingMiddleware } from '~/middlewares/errorHandlingMiddleware'
 
 const START_SERVER = () => {
   const app = express()
+
+  app.use(cors())
 
   //Enable req.body json data
   app.use(express.json())
